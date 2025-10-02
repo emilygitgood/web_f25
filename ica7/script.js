23704ffb3cd20c5036eb97c9526686ae033d409c
emilygitgood <mmunicorn12@gmail.com>
2025-10-02 17:58:46 -0600

    ica7

--- ica7/script.js
+++ ica7/script.js
@@ -17,3 +17,19 @@ filterCheckbox.addEventListener('change', () => {
   }
 });
 
+const form = document.getElementById('myForm');
+
+const savedData = JSON.parse(localStorage.getItem('formData') || '{}');
+for (let[name, value] of Object.entries(savedData)) {
+  if (form.elements[name]){
+    form.elements[name].value = value;
+  }
+}
+
+form.addEventListener('input', function(e) {
+  const data = JSON.parse(localStorage.getItem('formData') || '{}');
+
+  data[e.target.name] = e.target.value;
+
+  localStorage.setItem('formData', JSON.stringify(data));
+});
